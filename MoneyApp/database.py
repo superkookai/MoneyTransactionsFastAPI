cb70766cef523ae75399b3619eb9ec7abcb945ee
Weerawut <weerawut@live.com>
2025-02-04 16:48:16 +0700

    Change database to Postgresql

--- MoneyApp/database.py
+++ MoneyApp/database.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./moneyapp.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./moneyapp.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# Postgresql
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/MoneyAppDatabase"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
